e7f67d092c1e8f5aebb11f186cc17fc34a0dc6be
Seweryn Kuliga <skuligas@gmail.com>
2021-07-07 18:39:30 +0200

    Update

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyApzLEsJd4anEakaYusEFmZ805e_ApkhhE",
+    authDomain: "facebook-clone-f8174.firebaseapp.com",
+    projectId: "facebook-clone-f8174",
+    storageBucket: "facebook-clone-f8174.appspot.com",
+    messagingSenderId: "578753954460",
+    appId: "1:578753954460:web:218cb8dc51a071a058e359",
+    measurementId: "G-PB2E7ZFL7G"
+  };
--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build the facebook clone!</h1>
+
+      {/* Header */}
+
+      {/* App body */}
+
+      {/* Feed */}
+
+      {/* widgets */}
+
     </div>
   );
 }
