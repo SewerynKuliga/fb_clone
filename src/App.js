441dbcae2b755d425e3e67718b858f2c58e5a24a
Seweryn Kuliga <skuligas@gmail.com>
2021-07-12 13:39:06 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
@@ -8,11 +9,13 @@ function App() {
 
       <Header />
 
-      {/* App body */}
 
-      {/* Feed */}
+      <div className="app__body">
+        <Sidebar /> 
+        {/* Feed */}
 
-      {/* widgets */}
+        {/* widgets */}
+      </div>
 
     </div>
   );
