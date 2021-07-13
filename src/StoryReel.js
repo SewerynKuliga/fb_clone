db1c93aea6897838d76587e8ba667c58e4bee7fa
Seweryn Kuliga <skuligas@gmail.com>
2021-07-13 11:02:24 +0200

    Update

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./StoryReel.css";
+import Story from "./Story.js"
+
 
 function StoryReel() {
     return (
         <div className="storyReel">
-
+            <Story image="" profileSrc="" title=""/>
+            <Story image="" profileSrc="" title=""/>
+            <Story image="" profileSrc="" title=""/>
+            <Story image="" profileSrc="" title=""/>
+            <Story image="" profileSrc="" title=""/>
+            <Story image="" profileSrc="" title=""/>
         </div>
     )
 }
