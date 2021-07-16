c1bac532537cce6e85ca0bd0266a9c30a9db0969
Seweryn Kuliga <skuligas@gmail.com>
2021-07-16 13:20:39 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
   return (
     <div className="app">
-
       <Header />
 
-      
       <div className="app__body">
-        <Sidebar /> 
+        <Sidebar />
         <Feed />
-        {/* widgets */}
+        <Widgets />
       </div>
 
     </div>
