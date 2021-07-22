787baa4c30914d15ea78750e437f2e4ba537357d
Seweryn Kuliga <skuligas@gmail.com>
2021-07-22 12:04:03 +0200

    Update

--- src/Widgets.js
+++ src/Widgets.js
@@ -8,12 +8,12 @@ function Widgets() {
                 title="widget"
                 src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=360&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
                 width="340"
-            height="100%"
-            style={{ border: "none", overflow: "hidden" }}
-            scrolling="no"
-            frameBorder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
+                height="100%"
+                style={{ border: "none", overflow: "hidden" }}
+                scrolling="no"
+                frameBorder="0"
+                allowtransparency="true"
+                allow="encrypted-media"
             ></iframe>
         </div>
     )
